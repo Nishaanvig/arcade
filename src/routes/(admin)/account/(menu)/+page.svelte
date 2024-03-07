cd4c2b91e21173c02ab5787d8a56b811941ea94f
Nishaan Vigneswaran <120536003+Nishaanvig@users.noreply.github.com>
2024-03-07 17:38:42 +1100

    Remove position stats from dashboard page

--- src/routes/(admin)/account/(menu)/+page.svelte
+++ src/routes/(admin)/account/(menu)/+page.svelte
@@ -11,18 +11,3 @@
 </svelte:head>
 
 <h1 class="text-2xl font-bold mb-1">Dashboard</h1>
-
-<div class="my-6">
-  <h1 class="text-xl font-bold mb-1">Position</h1>
-  <div class="stats shadow stats-vertical sm:stats-horizontal sm:w-[420px]">
-    <div class="stat place-items-center">
-      <div class="stat-title">Today</div>
-      <div class="stat-value">54</div>
-    </div>
-
-    <div class="stat place-items-center">
-      <div class="stat-title">Streak</div>
-      <div class="stat-value text-secondary">5 day streak</div>
-    </div>
-  </div>
-</div>
